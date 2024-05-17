a8d74da56f9f97dc1ce9d478953636ab4c87d565
Adeyemo Ademola <adeyemoademola12@gmail.com>
2024-05-17 18:16:45 +0100

    Deploying to vercel

--- src/component/chart/TradingViewWidget.tsx
+++ src/component/chart/TradingViewWidget.tsx
@@ -1,46 +1,46 @@
-// TradingViewWidget.jsx
-import { useEffect, useRef, memo } from "react";
+// // TradingViewWidget.jsx
+// import { useEffect, useRef, memo } from "react";
 
-function TradingViewWidget() {
-  const container = useRef();
+// function TradingViewWidget() {
+//   const container = useRef();
 
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src =
-      "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
-    script.type = "text/javascript";
-    script.async = true;
-    script.innerHTML = `
-        {
-          "width": "100%",
-          "height": "610",
-          "symbol": "NASDAQ:AAPL",
-          "interval": "D",
-          "timezone": "Etc/UTC",
-          "theme": "light",
-          "style": "1",
-          "locale": "en",
-          "allow_symbol_change": true,
-          "calendar": false,
-          "support_host": "https://www.tradingview.com"
-        }`;
-    container.current.appendChild(script);
-  }, []);
+//   useEffect(() => {
+//     const script = document.createElement("script");
+//     script.src =
+//       "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
+//     script.type = "text/javascript";
+//     script.async = true;
+//     script.innerHTML = `
+//         {
+//           "width": "100%",
+//           "height": "610",
+//           "symbol": "NASDAQ:AAPL",
+//           "interval": "D",
+//           "timezone": "Etc/UTC",
+//           "theme": "light",
+//           "style": "1",
+//           "locale": "en",
+//           "allow_symbol_change": true,
+//           "calendar": false,
+//           "support_host": "https://www.tradingview.com"
+//         }`;
+//     container.current.appendChild(script);
+//   }, []);
 
-  return (
-    <div className="tradingview-widget-container" ref={container}>
-      <div className="tradingview-widget-container__widget"></div>
-      <div className="tradingview-widget-copyright">
-        <a
-          href="https://www.tradingview.com/"
-          rel="noopener nofollow"
-          target="_blank"
-        >
-          <span className="blue-text">Track all markets on TradingView</span>
-        </a>
-      </div>
-    </div>
-  );
-}
+//   return (
+//     <div className="tradingview-widget-container" ref={container}>
+//       <div className="tradingview-widget-container__widget"></div>
+//       <div className="tradingview-widget-copyright">
+//         <a
+//           href="https://www.tradingview.com/"
+//           rel="noopener nofollow"
+//           target="_blank"
+//         >
+//           <span className="blue-text">Track all markets on TradingView</span>
+//         </a>
+//       </div>
+//     </div>
+//   );
+// }
 
-export default memo(TradingViewWidget);
+// export default memo(TradingViewWidget);
