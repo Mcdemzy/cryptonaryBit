23392ca6045aa2e21c09be2a9b2c1495861d63d4
Adeyemo Ademola <adeyemoademola12@gmail.com>
2024-05-18 12:32:07 +0100

    Deploying to vercel

--- src/component/chart/TradingViewWidget.tsx
+++ src/component/chart/TradingViewWidget.tsx
@@ -1,30 +1,31 @@
-// TradingViewWidget.jsx
 import { useEffect, useRef, memo } from "react";
 
 function TradingViewWidget() {
-  const container = useRef();
+  const container = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    const script = document.createElement("script");
-    script.src =
-      "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
-    script.type = "text/javascript";
-    script.async = true;
-    script.innerHTML = `
-        {
-          "width": "100%",
-          "height": "610",
-          "symbol": "NASDAQ:AAPL",
-          "interval": "D",
-          "timezone": "Etc/UTC",
-          "theme": "light",
-          "style": "1",
-          "locale": "en",
-          "allow_symbol_change": true,
-          "calendar": false,
-          "support_host": "https://www.tradingview.com"
-        }`;
-    container.current.appendChild(script);
+    if (container.current) {
+      const script = document.createElement("script");
+      script.src =
+        "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
+      script.type = "text/javascript";
+      script.async = true;
+      script.innerHTML = `
+          {
+            "width": "100%",
+            "height": "610",
+            "symbol": "NASDAQ:AAPL",
+            "interval": "D",
+            "timezone": "Etc/UTC",
+            "theme": "light",
+            "style": "1",
+            "locale": "en",
+            "allow_symbol_change": true,
+            "calendar": false,
+            "support_host": "https://www.tradingview.com"
+          }`;
+      container.current.appendChild(script);
+    }
   }, []);
 
   return (
