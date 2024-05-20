25b836a933fabe548246c7dadb9286849649d7de
Adeyemo Ademola <adeyemoademola12@gmail.com>
2024-05-20 18:39:51 +0100

    Deploying to vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import EtfFinancials from "./component/etf/EtfFinancials";
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "./component/landingpage/LandingPage";
+import Signup from "./component/signup/Signup";
+import Signin from "./component/signin/Signin";
+// import Navbar from "./component/landingpage/components/Navbar";
 
 const App = () => {
   return (
-    <div>
-      <EtfFinancials />
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+      </Routes>
+      {/* <LandingPage /> */}
+    </>
   );
 };
 
