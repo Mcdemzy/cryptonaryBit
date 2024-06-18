4c87ec89dc2e2ef72ab2e45a631c2dc73b68af41
Adeyemo Ademola <adeyemoademola12@gmail.com>
2024-06-18 10:06:51 +0100

    Fixed Github Issue

--- src/component/sendEmail.ts
+++ src/component/sendEmail.ts
@@ -1,7 +1,7 @@
 import mailgun from "mailgun-js";
 
 const mg = mailgun({
-  apiKey: "3350b891fd4045c447615e23051b53c0-6fafb9bf-ddd341c4", // Replace with your Mailgun API key
+  apiKey: "@@@", // Replace with your Mailgun API key
   domain: "sandboxa786c66722c04f1ea6b5c9af7e929538.mailgun.org", // Replace with your Mailgun domain
   testMode: false, // Set to true to enable Mailgun's test mode for debugging
 });
