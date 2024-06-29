17eed148b1e4bb1cce779dcb7c566dcfa3c44acf
Adeyemo Ademola <adeyemoademola12@gmail.com>
2024-06-29 21:12:55 +0100

    Fixed Backend Issue

--- utils/services.ts
+++ utils/services.ts
@@ -1,6 +1,6 @@
 export const BASE_URL =
   process.env.NODE_ENV == "production"
-    ? "https://crypto-app-backend-sage.vercel.app/"
+    ? "https://crypto-app-backend-sage.vercel.app"
     : "http://localhost:5000";
 
 import { type Transactions } from "../src/component/deposit/BTCDeposit";
