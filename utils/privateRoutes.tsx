11525aeae9363fb60480aadb0d8bbbffa8b1fcba
Adeyemo Ademola <adeyemoademola12@gmail.com>
2024-06-30 20:13:38 +0100

    Improved Private Routes

--- utils/privateRoutes.tsx
+++ utils/privateRoutes.tsx
@@ -7,7 +7,14 @@ const PrivateRoutes = ({ children }: { children: ReactNode }) => {
   const { isAuth, loading } = useAuthContext();
 
   if (loading) {
-    return <Loader size={40} strokeWidth={1.5} />;
+    return (
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <div className="flex flex-col items-center">
+          <Loader size={40} strokeWidth={1.5} className="animate-spin" />
+          <span className="mt-4 text-gray-700">Loading...</span>
+        </div>
+      </div>
+    );
   }
 
   if (isAuth) {
